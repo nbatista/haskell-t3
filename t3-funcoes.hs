67176717f98597bf41ae870001d254e03e7c4dc8
nbatista <nbatista@inf.ufsm.br>
2015-04-06 22:32:04 -0300

    questão 3

--- t3-funcoes.hs
+++ t3-funcoes.hs
@@ -11,3 +11,14 @@ firstName str
 -- Questão 2
 firstName' :: String -> String
 firstName' str = takeWhile (/= ' ') str
+
+-- Questão 3
+lastName :: String -> String
+lastName [] = []
+lastName str = reverse (lastNameAux str)
+
+lastNameAux :: String -> String
+lastNameAux [] = []
+lastNameAux str
+	| last str == ' ' = []
+	| otherwise = last str : lastNameAux (init str)
